5b2af3fd7536f78bf595d1e44f0d345b4ec65908
IshMag <ke1613ine46@susu.ru>
2021-04-16 23:27:43 +0500

    исправлено форматирование файла mycomplex.h

--- testcmp.cpp
+++ testcmp.cpp
@@ -2,24 +2,24 @@
 #include "mycomplex.h"
 
 using namespace std;
+
 int main()
 {
-    //тестирование класса комплексных чисел
-    Complex A,B(-4), C(23.0, 45.9);
-
+    Complex A, B(-4), C(23.0, 45.9);
     cout << A << "," << B << "," << C << endl;
     A = B + C;
-    cout << " A = B + C ,A="<< A <<endl;
+    cout << "A = B + C, A = " << A << endl;
 
-    Complex M=B-C;
+    Complex M = B - C;
     cout << "M = B - C, M = " << M << endl;
-    cout << "M*A=" << M * A << endl;
-    cout << " M / 4.45 = " << M/4.45 <<endl;
+    cout << " M * A = " << M * A << endl;
+    cout << " M / 4.45= " << M / 4.45 << endl;
 
     Complex D;
-  	cout << "Enter complex number D = ";
-  	cin >> D;
+    cout << "Enter complex number D = ";
+    cin >> D;
     A += C + D;
     cout << "D = " << D << "\nA = " << A << endl;
+
     return 0;
 }
